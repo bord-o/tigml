59db812d9e3957ccc86f9f2d7a5b0466636a2bb2
bordo <brodylittle011@gmail.com>
2023-08-15 21:48:28 -0400

    port the symbol table from sml book code

---
+++ lib/types.ml
@@ -0,0 +1,12 @@
+open Symbol
+
+type unique = unit ref
+
+type ty =
+  | RECORD of (Symbol.symbol * ty) list * unique
+  | NIL
+  | INT
+  | STRING
+  | ARRAY of ty * unique
+  | NAME of Symbol.symbol * ty option ref
+  | UNIT
